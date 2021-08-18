ea1255766b20fdc93f84d865fbef211debd67a89
Naveed Zaheer <nzaheer@microsoft.com>
2021-08-18 08:42:57 -0500

    first update

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace fdwebdemo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Location { get; set;}
+        private IConfigurationRoot ConfigRoot;
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(IConfiguration configRoot, ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            ConfigRoot = (IConfigurationRoot)configRoot;
+        }
+
+        public void OnGet()
+        {
+            Location = ConfigRoot["Location"];
+        }
+    }
+}
